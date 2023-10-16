58e2211e0ed185b48de4a7041cca2dacef8256a6
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-10-16 19:50:15 +0300

    add-render-item-prop

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,21 @@ function App() {
         data={[
           { id: 1, name: "David", role: "Manager" },
           { id: 2, name: "Angella", role: "Developer" },
+          { id: 3, name: "Faniya", role: "UX" },
+          { id: 4, name: "Rustem", role: "Developer" },
+          { id: 5, name: "Alex", role: "Developer" },
+          { id: 6, name: "Jarommy", role: "QA" },
         ]}
         id="id"
         primary="name"
         secondary="role"
+        style={{ width: "300px", maxHeight: "380px", overflowY: "auto" }}
+        renderItem={(item) => (
+          <li key={item.id} className="bg-white p-4 border-b-2">
+            <div className="text-xl text-slate-800 pb-1">{item.name}</div>
+            <div className="text-slate-500">{item.role}</div>
+          </li>
+        )}
       />
     </div>
   );
--- src/Checklist/Checklist.tsx
+++ src/Checklist/Checklist.tsx
@@ -1,14 +1,27 @@
+import { ComponentPropsWithRef, ReactNode } from "react";
+
 type Props<Data> = {
   data: Data[];
   id: keyof Data;
   primary: keyof Data;
   secondary: keyof Data;
-};
+  renderItem?: (item: Data) => ReactNode;
+} & ComponentPropsWithRef<"ul">;
 
-export function Checklist<Data>({ data, id, primary, secondary }: Props<Data>) {
+export function Checklist<Data>({
+  data,
+  id,
+  primary,
+  secondary,
+  renderItem,
+  ...ulProps
+}: Props<Data>) {
   return (
-    <ul className="bg-gray-300 rounded p-10">
+    <ul className="bg-gray-300 rounded p-10" {...ulProps}>
       {data.map((item) => {
+        if (renderItem) {
+          return renderItem(item);
+        }
         const idValue = item[id] as unknown;
         if (typeof idValue !== "string" && typeof idValue !== "number") {
           return null;
